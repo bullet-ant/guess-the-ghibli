c7d61ebc9588ed5ade79f76ee97c1722e10c2fd0
bullet-ant <aman.kr.2129@gmail.com>
2025-02-16 19:17:07 +0530

    add dotenv support and configure GraphQL server URI in environment variables

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  uri: import.meta.env.VITE_GRAPHQL_SERVER_URI,
   cache: new InMemoryCache(),
 });
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
